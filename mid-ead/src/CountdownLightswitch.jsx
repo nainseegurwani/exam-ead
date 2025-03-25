dcd53929cd78932075de64d490c9ec3b7b676bf4
nainseegurwani <gurwaninainsee@gmail.com>
2025-03-25 11:23:57 -0700

    initial-commit

---
+++ mid-ead/src/CountdownLightswitch.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function CountdownLightswitch (){
+    
+    
+    return(
+     <h1 style={{color:"red",background:"yellow", width:"500px",height: "300px"}}>Count &Lightswitch</h1>
+     
+
+
+    )
+    
+}
+export default CountdownLightswitch;
